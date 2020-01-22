4d8cf6e11567369e20635ea81f9e454fe4e9d1ff
Isaura Almar Rios <you@example.com>
2020-01-22 11:37:57 +0100

    adding: buildspec.yml and helloWorld.cs

---
+++ helloWorld.cs
@@ -0,0 +1,13 @@
+// these is a testst 
+
+
+using System;
+
+namespace HelloWorld
+{
+    class Geeks{
+        static void Main (string[] args){
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
